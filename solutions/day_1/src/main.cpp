4edcc90908497510f70da2714dff9f2fb4e63ac8
CharlieCurious <dfrco2@outlook.com>
2024-12-22 23:48:20 +0200

    [day 1] some improvements for a noob and time benchmark.

--- solutions/day_1/src/main.cpp
+++ solutions/day_1/src/main.cpp
@@ -1,47 +1,50 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <ostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 #include <unordered_map>
 
-using namespace std;
+#define INPUT_LINE_COUNT 1000
 
 int main() {
-    const string fileName = "input_files/day_1.txt"; 
-    ifstream file(fileName);
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    const std::string fileName = "input_files/day_1.txt"; 
+    std::ifstream file(fileName);
     if (!file.is_open()) {
-        cerr << "Failed to open the file." << endl;
+        std::cerr << "Failed to open the file." << std::endl;
         return EXIT_FAILURE;
     }
 
-    vector<int32_t> leftCol;
-    vector<int32_t> rightCol;
-    unordered_map<int32_t, int32_t> multipliers;
+    std::array<int32_t, INPUT_LINE_COUNT> leftCol;
+    std::array<int32_t, INPUT_LINE_COUNT> rightCol;
+    std::unordered_map<int32_t, int32_t> multipliers;
 
-    string line;
+    std::string line;
     uint32_t lineCount = 0;
-    while (getline(file, line)) {
-        istringstream iss(line);
+    while (getline(file, line) && lineCount < INPUT_LINE_COUNT) {
+        std::istringstream iss(line);
         int32_t leftNum, rightNum;
         if (iss >> leftNum >> rightNum) {
-            leftCol.push_back(leftNum);
-            rightCol.push_back(rightNum);
+            leftCol[lineCount] = leftNum;
+            rightCol[lineCount] = (rightNum);
 
             multipliers[rightNum]++;
+            lineCount++;
         } else {
-            cerr << "Invalid line format: " << line << endl;
+            std::cerr << "Invalid line format: " << line << std::endl;
             return EXIT_FAILURE;
         }
 
-        lineCount++;
     }
 
-    sort(leftCol.begin(), leftCol.end());
-    sort(rightCol.begin(), rightCol.end());
+    std::sort(leftCol.begin(), leftCol.end());
+    std:: sort(rightCol.begin(), rightCol.end());
 
     int32_t part1Sum = 0;
     int32_t part2Sum = 0;
@@ -52,8 +55,13 @@ int main() {
         part2Sum += multipliers[leftNum] == 0 ? 0 : leftNum * multipliers[leftNum];
     }
 
-    cout << "Part 1: " << part1Sum << endl;
-    cout << "Part 2: " << part2Sum << endl;
+    auto endTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsedSeconds = endTime - startTime;
+
+    std::cout << "Part 1: " << part1Sum << "\n";
+    std::cout << "Part 2: " << part2Sum << "\n";
+
+    std::cout << "Execution time: " << elapsedSeconds.count() << "seconds\n";
 
     return EXIT_SUCCESS;
 }
